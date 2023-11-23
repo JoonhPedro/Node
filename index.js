c0e160a573ae1c15521b69329fe921e961a90372
JonhBala <joao.pereira@gsd.com.br>
2023-11-23 14:01:13 -0300

    Adicionando Router

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 
-
-
 //Vai subir a aplicação !
 const app = express()
 
+app.get("/", (request, response, next) => {
+    response.json({
+        message: "Show"
+    })
+})
 
 app.listen(process.env.PORT, () => {
     console.log("App está rodando!")
